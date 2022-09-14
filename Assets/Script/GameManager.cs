12379b3e02906cf2514aea93006b98ba163ba5bd
Artyu24 <arthurpejoiane@hotmail.fr>
2022-09-14 17:10:45 +0200

    Add Some Script

---
+++ Assets/Script/GameManager.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance;
+
+    [SerializeField] private Grid ldGrid;
+    private float moveDistance = 1;
+    public float GetMoveDistance => moveDistance;
+
+    private GameState actualGameState = GameState.Start;
+    public GameState ActualGameState { get => actualGameState; set => actualGameState = value; }
+
+    void Awake()
+    {
+        if (Instance == null)
+            Instance = this;
+
+        ldGrid = FindObjectOfType<Grid>();
+        moveDistance = ldGrid.cellSize.x;
+    }
+}
