2a9cff0d06d55494a379d6b170cead518785181b
Tos927 <t.sautereau92@gmail.com>
2022-10-21 15:50:24 +0200

    Test DataPoke

--- Assets/Script/CSVDownloader.cs
+++ Assets/Script/CSVDownloader.cs
@@ -1,18 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.Networking;
 
 public class CSVDownloader : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    private const string k_googleSheetDocID = "1QAGetZagLebbvVikbkj5g_EMedwKmqMZJLn6wQmnmKA";
+    private const string url = "https://docs.google.com/spreadsheets/d/" + k_googleSheetDocID + "/export?format=csv";
 
-    // Update is called once per frame
-    void Update()
+    internal static IEnumerator DownloadData(System.Action<string> onCompleted)
     {
-        
+        yield return new WaitForEndOfFrame();
+
+        string downloadData = null;
+        using (UnityWebRequest webRequest = UnityWebRequest.Get( url ))
+        {
+            yield return webRequest.SendWebRequest();
+
+            if (webRequest.isNetworkError)
+            {
+                Debug.Log("Download Error : " + webRequest.error);
+                downloadData = PlayerPrefs.GetString("LastDataDownloaded", null);
+                string versionText = PlayerPrefs.GetString("LastDataDownloaded", null);
+            }
+        }
     }
 }
