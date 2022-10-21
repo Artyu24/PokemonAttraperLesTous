17e6fc780bb81de948678bd0f2dd66dbb536d9ad
Tos927 <t.sautereau92@gmail.com>
2022-10-21 13:32:30 +0200

    Update Herbres Hautes

--- Assets/Script/HerbesHautes.cs
+++ Assets/Script/HerbesHautes.cs
@@ -5,61 +5,30 @@ using UnityEngine;
 
 public class HerbesHautes : MonoBehaviour
 {
+    [SerializeField]
     private int spawnRate;
-    private Sprite movementSprite;
-    private BoxCollider2D herbeCollier;
+    [SerializeField]
+    private int maxSpawnRate;
 
     public GameObject combatWindow;
     public PokeData[] wildPokes = new PokeData[4];
     public PokeData wildPoke;
 
-    private bool inGrass = false;
+    private PokeData WildPoke { get => wildPoke; }
 
-    public PokeData WildPoke { get => wildPoke; }
-
-
-    void Awake()
-    {
-        herbeCollier = GetComponent<BoxCollider2D>();
-    }
-    
     void Start()
     {
-        spawnRate = Random.Range(0, 30);
-
         for (int i = 0; i < wildPokes.Length; i++)
         {
             wildPokes[i] = CombatManager.Instance.DictPokeData[Random.Range(0, 6)];
         }
     }
-
-    public void OnTriggerEnter2D(Collider2D other)
+    
+    public void SpawnPokemon()
     {
-        if (other.CompareTag("Player"))
+        int random = Random.Range(0, maxSpawnRate);
+        if (random <= spawnRate)
         {
-            SpawnPokemon();
-        }
-    }
-    private void OnTriggerExit2D(Collider2D other)
-    {
-
-        if (other.CompareTag("Player"))
-        {
-            if (!inGrass)
-                inGrass = true;
-            else
-            {
-                inGrass = false;
-            }
-
-        }
-    }
-
-    private void SpawnPokemon()
-    {
-        if (Random.Range(1, 101) <= spawnRate)
-        {
-            spawnRate = Random.Range(0, 100);
             int temp = Random.Range(0, wildPokes.Length);
             wildPoke = wildPokes[temp];
             combatWindow.SetActive(true);
@@ -67,10 +36,6 @@ public class HerbesHautes : MonoBehaviour
             GameManager.Instance.ActualGameState = GameState.Fight;
             CombatManager.Instance.ActualCombatState = CombatState.Init;
             CombatManager.Instance.StartCombat(wildPoke);
-        }
-        else
-        {
-            spawnRate = Random.Range(0, 100);
         }
     }
 }
